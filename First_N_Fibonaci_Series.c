3260cd195b24b338ae054d8d4aeeb12e1acd4464
Suraj Kumar <surajkumarjnv35@gmail.com>
2025-09-07 19:03:56 +0530

    Create First_N_Fibonaci_Series.c

---
+++ First_N_Fibonaci_Series.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 0, b = 1, c, n, i;
+    printf("Enter the value of terms: \n");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        printf("%d, ", a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
